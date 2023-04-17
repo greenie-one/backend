11f8a8cb79d4086b39bf2157f516decb0cf78b67
Ovenoboyo <ovenoboyo@gmail.com>
2023-04-17 21:16:06 +0530

    Misc changes
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,4 +5,5 @@ const password = encodeURIComponent(env('DB_PASSWORD'));
 const host = env('DB_HOST');
 const database = env('DB_DATABASE');
 
-export const dbConnection = `mongodb+srv://${user}:${password}@${host}/${database}?replicaSet=rs0&tls=false&authMechanism=DEFAULT`;
+export const dbConnection = `mongodb+srv://${user}:${password}@${host}/${database}?replicaSet=rs0&tls=false&authMechanism=DEFAULT&authSource=${database}`;
+console.log(dbConnection);
--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,10 @@ import { AuthController } from './controllers/auth.controller';
 import EducationHistoryController from './controllers/education.controller';
 import ProfileController from './controllers/profile.controller';
 import RootController from './controllers/root.controller';
-import UserController from './controllers/users.controller';
 
 ValidateEnv();
 
-const controllers = [RootController, AuthController, UserController, ProfileController, EducationHistoryController];
+const controllers = [RootController, AuthController, ProfileController, EducationHistoryController];
 
 const app = new App(controllers);
 
