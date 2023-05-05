ffd728c70b1b651340849b89412ddea98bdb85a7
Ratnesh <ratneshjain40@gmail.com>
2023-05-05 20:17:18 +0530

    Eliminate Class validator

--- src/remote/generic/emailer.ts
+++ src/remote/generic/emailer.ts
@@ -1,25 +1,15 @@
 import { env } from '@/config';
-import { plainToInstance } from 'class-transformer';
-import { IsNotEmpty, IsString, validate } from 'class-validator';
 import fs from 'fs';
 import nodemailer from 'nodemailer';
 
-export class Message {
-  @IsString()
-  @IsNotEmpty()
+interface Message {
   to: string;
-
-  @IsString()
-  @IsNotEmpty()
   from: string;
-
-  @IsString()
   subject: string;
-
   text: string;
 }
 
-export class Mailer {
+class Mailer {
   private transporter: nodemailer.Transporter;
 
   constructor() {
@@ -41,11 +31,6 @@ export class Mailer {
   }
 
   public async sendMail(mailOptions: Message) {
-    if (!(mailOptions instanceof Message)) {
-      mailOptions = plainToInstance(Message, mailOptions as Message);
-    }
-
-    await validate(mailOptions);
     return this.transporter.sendMail(mailOptions);
   }
 }
