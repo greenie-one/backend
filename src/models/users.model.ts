d9de1dd5d78ee71526faa543a8cfba98892a7a83
Ratnesh <ratneshjain40@gmail.com>
2023-05-06 18:35:57 +0530

    User Model Updates

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -10,16 +10,22 @@ export class User {
   public _id?: string;
 
   @prop({ type: String, required: false, unique: true, sparse: true })
-  public email: string;
+  public email?: string;
 
   @prop({ type: String, required: false, unique: true, sparse: true })
-  public mobileNumber: string;
+  public mobileNumber?: string;
+
+  @prop({ type: String, required: false })
+  public firstName?: string;
+
+  @prop({ type: String, required: false })
+  public lastName?: string;
 
   @prop({ type: String, required: false })
   public password?: string;
 
   @prop({ type: String, enum: UserRoles, required: true, default: [UserRoles.DEFAULT] })
-  public roles!: UserRoles[];
+  public roles?: UserRoles[];
 }
 
 export const UserModel = getModelForClass(User);
