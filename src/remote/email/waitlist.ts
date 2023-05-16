e49a0bd4c607ab8f4804ab4460daf81ac52e9262
Ratnesh <ratneshjain40@gmail.com>
2023-05-16 19:36:55 +0530

    Changes to templating

--- src/remote/email/waitlist.ts
+++ src/remote/email/waitlist.ts
@@ -3,12 +3,12 @@ import { mailer } from '../generic/emailer';
 
 export class WaitlistMailer {
   static async sendMail(firstName: string, email: string) {
-    const text = await ejs.renderFile('templates/waitlist.ejs', { firstName });
+    const html = await ejs.renderFile('templates/waitlist.ejs', { firstName });
     return mailer.sendMail({
       from: 'office@greenie.one',
       to: email,
       subject: 'Added to Greenie Waitlist!',
-      text,
+      html,
     });
   }
 }
