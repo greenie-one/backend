1040e240b3d37ced0d18da2332f5e68f35c8e02f
Ratnesh <ratneshjain40@gmail.com>
2023-05-16 23:17:05 +0530

    Email name changed to - Greenie

--- src/remote/email/waitlist.ts
+++ src/remote/email/waitlist.ts
@@ -5,7 +5,7 @@ export class WaitlistMailer {
   static async sendMail(firstName: string, email: string) {
     const html = await ejs.renderFile('templates/waitlist.ejs', { firstName });
     return mailer.sendMail({
-      from: 'office@greenie.one',
+      from: 'Greenie <office@greenie.one>',
       to: email,
       subject: 'Added to Greenie Waitlist!',
       html,
