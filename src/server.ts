b9bcf78050f9f0ef5c1bb45fbcc663d6138936f9
dharmikjethva30 <dharmikjethva4@gmail.com>
2023-06-14 18:05:45 +0530

    Document upload enpoints

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { App } from '@/app';
 import { ValidateEnv } from '@utils/validateEnv';
+import DocumentController from './controllers/document.controller';
 import IDsController from './controllers/ids.controller';
 import { OAuthController } from './controllers/oauth.controller';
 import ProfileController from './controllers/profile.controller';
@@ -22,6 +23,7 @@ process.on('unhandledRejection', (e) => {
 ValidateEnv();
 
 const controllers = [
+  DocumentController,
   RootController,
   OAuthController,
   ProfileController,
