656436c927bf52fa13349dcfc8e9a6d8cd4f609d
dharmikjethva30b <dharmikjethva30@gmail.com>
2023-06-22 21:36:46 +0530

    PubSUb model made and minor changes

---
+++ src/redisClient/deleteService.ts
@@ -0,0 +1,14 @@
+import { redisUtilClient } from './index';
+
+class redisPUBSUB {
+  public async docDelete(fileName: string, containerName: string) {
+    const deleteMessage = {
+      file_name: fileName,
+      container_name: containerName,
+    };
+    await redisUtilClient.publish('doc_delete', JSON.stringify(deleteMessage));
+    console.log('Message Published');
+  }
+}
+
+export const RedisPUBSUB = new redisPUBSUB();
