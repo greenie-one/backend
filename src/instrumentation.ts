320afd2c10d085c0ca8a1b60e0658190ebc9e5d8
Sahil Gupte <ovenoboyo@gmail.com>
2023-06-30 16:57:57 +0530

    use grpc endpoints

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,7 +1,7 @@
 import { env } from '@/config';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
@@ -10,7 +10,7 @@ const sdk = new NodeSDK({
   serviceName: `core-${env('APP_ENV')}`,
   autoDetectResources: true,
   traceExporter: new OTLPTraceExporter({
-    url: `${env('OTEL_URL')}/v1/traces`,
+    url: `${env('OTEL_URL')}`,
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
