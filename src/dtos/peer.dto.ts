cf493e5a242bd51690c8f4dc5f859aae144fc33a
sumaiya-cmd <88318661+sumaiya-cmd@users.noreply.github.com>
2023-07-05 17:22:05 +0530

    dtos added

--- src/dtos/peer.dto.ts
+++ src/dtos/peer.dto.ts
@@ -1,5 +1,8 @@
+import { DocumentType } from '@/models/document.model';
+import { skillTypeEnum } from '@/models/skills.model';
 import { PeerType, State } from '@models/peer.model';
-import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsEnum, IsNotEmpty, IsOptional, IsString, ValidateNested } from 'class-validator';
 
 export class CreatePeerDto {
   @IsString()
@@ -23,8 +26,83 @@ export class CreatePeerDto {
   public workExperience!: string;
 }
 
-export class PeerSkillVerificationDto {
+export class SkillDto {
+  @IsString()
+  @IsNotEmpty()
+  public skillName!: string;
+
+  @IsEnum(skillTypeEnum)
+  @IsNotEmpty()
+  public expertise!: skillTypeEnum;
+
   @IsEnum(State)
   @IsNotEmpty()
   public state!: State;
+}
+
+export class DocumentDto {
+  @IsString()
+  @IsNotEmpty()
+  public documentName!: string;
+
+  @IsEnum(DocumentType)
+  @IsNotEmpty()
+  public documentType!: DocumentType;
+
+  @IsEnum(State)
+  @IsNotEmpty()
+  public state!: State;
+}
+
+export class FieldDto {
+  @IsNotEmpty()
+  public value!: string;
+
+  @IsEnum(State)
+  @IsNotEmpty()
+  public state!: State;
+}
+
+export class ExperienceDetailsDto {
+  @ValidateNested()
+  @Type(() => FieldDto)
+  public candidateId!: FieldDto;
+
+  @ValidateNested()
+  @Type(() => FieldDto)
+  public department!: FieldDto;
+
+  @ValidateNested()
+  @Type(() => FieldDto)
+  public designation!: FieldDto;
+
+  @ValidateNested()
+  @Type(() => FieldDto)
+  public dateOfJoining!: FieldDto;
+
+  @ValidateNested()
+  @Type(() => FieldDto)
+  public dateOfLeaving!: FieldDto;
+}
+
+export class PeerVerificationResponse {
+  @ValidateNested()
+  @Type(() => CreatePeerDto)
+  @IsNotEmpty()
+  public peerDetails!: CreatePeerDto;
+
+  @ValidateNested()
+  @Type(() => ExperienceDetailsDto)
+  @IsNotEmpty()
+  public peerVerificationStatus!: ExperienceDetailsDto;
+
+  @ValidateNested({ each: true })
+  @Type(() => SkillDto)
+  @IsOptional()
+  public peerSkillStatus?: SkillDto[];
+
+  @ValidateNested({ each: true })
+  @Type(() => DocumentDto)
+  @IsOptional()
+  public peerDocumentStatus?: DocumentDto[];
 }
