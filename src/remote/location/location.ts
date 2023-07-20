9905af5e58260de02350a7429c147163889e7b11
Ratnesh <ratneshjain40@gmail.com>
2023-07-20 21:49:15 +0530

    Some code smells fixes

--- src/remote/location/location.ts
+++ src/remote/location/location.ts
@@ -1,9 +1,9 @@
 import { env } from '@/config';
-import { locationResponse } from '@/dtos/location.dto';
+import { LocationResponse } from '@/dtos/location.dto';
 import { HttpClient } from '../generic/httpClient';
 
 export class Geolocation {
-  static async getLocation(address: string): Promise<locationResponse> {
+  static async getLocation(address: string): Promise<LocationResponse> {
     return HttpClient.callApi({
       url: `${env('REMOTE_BASE_URL')}/location/geolocation`,
       method: 'POST',
