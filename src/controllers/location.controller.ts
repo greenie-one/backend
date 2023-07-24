63f9aa7d21ac7abfacdd00e67d477f75c0e6fde3
Ratnesh <ratneshjain40@gmail.com>
2023-07-24 14:50:46 +0530

    changes

--- src/controllers/location.controller.ts
+++ src/controllers/location.controller.ts
@@ -1,14 +1,14 @@
-import { IPLocation } from '../utils/decorators/location';
 import { GPScompare } from '@/dtos/request/location.dto';
-import { locationService } from '@/services/location.service';
 import { Controller } from '@/utils/decorators/controller';
 import { Post } from '@/utils/decorators/methods';
-import { Body } from '@/utils/decorators/request';
+import { Body, Params } from '@/utils/decorators/request';
+import { IPLocation } from '../utils/decorators/location';
 
 @Controller('/location')
 export default class LocationController {
-  @Post('/compare')
-  async compareIPandGPS(@IPLocation() ipLocation: string, @Body() gpsLocation: GPScompare) {
-    return locationService.compare(gpsLocation, ipLocation);
+  @Post('/verifyLcoation/:id')
+  public async verifyLocation(@Body() body: GPScompare, @Params('id') residentialInfoId: string, @IPLocation() IPlocation: string) {
+    // return await locationService.compare(body, IPlocation);
+    return;
   }
 }
