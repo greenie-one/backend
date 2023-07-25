ea7f4aded7583569a3e68294d399f58e1aea5f35
sumaiya-cmd <88318661+sumaiya-cmd@users.noreply.github.com>
2023-07-25 22:01:00 +0530

    inital

---
+++ src/dtos/request/skills.dto.ts
@@ -0,0 +1,28 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export enum SkillTypeEnum {
+  AMATEUR = 'AMATEUR',
+  EXPERT = 'EXPERT',
+  BEGINNER = 'BEGINNER',
+  SUPER_SPECIALIST = 'SUPER_SPECIALIST',
+  MASTER = 'MASTER',
+  HIGHLY_COMPETENT = 'HIGHLY_COMPETENT',
+}
+
+export class CreateSkillDto {
+  @IsString()
+  @IsNotEmpty()
+  public skillName!: string;
+
+  @IsString()
+  @IsOptional()
+  public workExperience?: string;
+
+  @IsEnum(SkillTypeEnum)
+  @IsNotEmpty()
+  public expertise!: SkillTypeEnum;
+}
+
+
+
+
