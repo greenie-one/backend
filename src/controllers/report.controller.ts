c1cc8fe67a1728d0fb78e623fb4b82be5e7aba41
sumaiya-cmd <88318661+sumaiya-cmd@users.noreply.github.com>
2023-08-14 17:36:25 +0530

    post

--- src/controllers/report.controller.ts
+++ src/controllers/report.controller.ts
@@ -1,11 +1,11 @@
 import { reportService } from "@/services/report.service";
 import { Controller } from "@/utils/decorators/controller";
-import { Get } from "@/utils/decorators/methods";
+import { Post } from "@/utils/decorators/methods";
 import { Body } from "@/utils/decorators/request";
 
 @Controller('/report')
 export default class reportController {
-  @Get('/')
+  @Post('/')
   async createReport(@Body() data: string) {
     return reportService.getAllDetials(data);
   }
