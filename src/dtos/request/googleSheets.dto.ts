cdf147e3f1bc9164f04abf05a7c2bbf471508033
Ratnesh <ratneshjain40@gmail.com>
2023-08-20 12:35:52 +0530

    google sheets

--- src/dtos/request/googleSheets.dto.ts
+++ src/dtos/request/googleSheets.dto.ts
@@ -1,6 +1,6 @@
 import { sanitizeMobileNumber } from '@/utils/validation';
 import { Transform } from 'class-transformer';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class addUserInfoDTO {
   @IsString()
@@ -17,4 +17,7 @@ export class addUserInfoDTO {
   @Transform((params) => sanitizeMobileNumber(params.value))
   public phone!: string;
 
+  @IsString()
+  @IsOptional()
+  public message?: string;
 }
