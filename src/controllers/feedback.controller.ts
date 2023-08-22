a1f1ce395aa34132213bf4fb7512b3812500acb5
Ratnesh <ratneshjain40@gmail.com>
2023-08-22 14:29:33 +0530

    roles

--- src/controllers/feedback.controller.ts
+++ src/controllers/feedback.controller.ts
@@ -1,16 +1,15 @@
 import { TokenClaims } from '@/dtos/request/auth.dto';
-import { AddFeedbackDto } from '@/dtos/request/feedback.dto';
+import { AddFeedbackDto, FeedbackType } from '@/dtos/request/feedback.dto';
 import { feedbackService } from '@/services/feedback.service';
 import { UserDetails } from '@/utils/decorators/auth';
 import { Controller } from '@/utils/decorators/controller';
 import { Get, Post } from '@/utils/decorators/methods';
 import { Body, Params } from '@/utils/decorators/request';
-import { FeedbackType } from "@/dtos/request/feedback.dto";
 
 @Controller('/feedback')
 export default class FeedbackController {
   @Post('/add')
-  public async addFeedback( @UserDetails() user: TokenClaims, @Body() body: AddFeedbackDto) {
+  public async addFeedback(@UserDetails() user: TokenClaims, @Body() body: AddFeedbackDto) {
     return await feedbackService.addFeedback(user.sub, body);
   }
 
--- src/utils/decorators/auth.ts
+++ src/utils/decorators/auth.ts
@@ -1,5 +1,8 @@
-export function UserDetails() {
+export function UserDetails(roles?: string[]) {
+  if (!roles) {
+    roles = ['default'];
+  }
   return function (target: unknown, property: string, index: number): void {
-    Reflect.defineMetadata('fastify:method:user_details', { index }, target, property);
+    Reflect.defineMetadata('fastify:method:user_details', { index, roles }, target, property);
   };
 }
