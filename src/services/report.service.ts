3f3acfc49a94fa54a6dcaf55b5df3456f833f307
sumaiya-cmd <88318661+sumaiya-cmd@users.noreply.github.com>
2023-08-22 13:48:52 +0530

    query phone added

--- src/services/report.service.ts
+++ src/services/report.service.ts
@@ -146,15 +146,16 @@ class ReportService {
     return idReportResponse;
   }
 
-  public async getAllDetails(email: string) {
-    const user = await UserModel.findOne({ email: email });
-    if (!user) throw new HttpException(ErrorEnum.USER_NOT_FOUND);
+  public async getAllDetails(email: string, phone:string) {
+    const user =await UserModel.findOne({email:email, mobileNumber:phone}) ;
 
+    if(!user)
+      throw new HttpException(ErrorEnum.USER_NOT_FOUND);
     return {
-      accountDetails: await this.getGreenieAccountDetails(email),
-      workExperienceDetails: await this.getWorkExperienceDetails(email),
-      ResidentialDetails: await this.getResidentialDetails(email),
-      idDetails: await this.getIdDetails(email),
+      accountDetails: await this.getGreenieAccountDetails(user._id),
+      workExperienceDetails: await this.getWorkExperienceDetails(user._id),
+      ResidentialDetails: await this.getResidentialDetails(user._id),
+      idDetails: await this.getIdDetails(user._id),
     };
   }
 }
