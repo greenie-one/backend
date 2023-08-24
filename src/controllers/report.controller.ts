d45e7d75b6b6c6f7d50170f748ec86bf8ae14084
Ratnesh <ratneshjain40@gmail.com>
2023-08-24 13:21:59 +0530

    report open

--- src/controllers/report.controller.ts
+++ src/controllers/report.controller.ts
@@ -1,6 +1,4 @@
-import { TokenClaims } from '@/dtos/request/auth.dto';
 import { reportService } from '@/services/report.service';
-import { UserDetails } from '@/utils/decorators/auth';
 import { Controller } from '@/utils/decorators/controller';
 import { Get } from '@/utils/decorators/methods';
 import { Query } from '@/utils/decorators/request';
@@ -8,7 +6,7 @@ import { Query } from '@/utils/decorators/request';
 @Controller('/report')
 export default class ReportController {
   @Get('')
-  async createReport(@UserDetails() _: TokenClaims, @Query('email', true) email?: string, @Query('phone', true) phone?: string) {
+  async createReport(@Query('email', true) email?: string, @Query('phone', true) phone?: string) {
     return reportService.getAllDetails(email, phone);
   }
 }
