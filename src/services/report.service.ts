1641e340d0c7e068a0557bc8f3681b07bcbaaf13
dharmikjethva30 <dharmikjethva4@gmail.com>
2023-09-14 15:36:08 +0530

    added Verification Status in report

--- src/services/report.service.ts
+++ src/services/report.service.ts
@@ -1,5 +1,5 @@
 import { IDTypeEnum } from '@/dtos/request/ids.dto';
-import { IdReportResonse, ResidentialReportResponse, ResidentialResponse, WorkExpReportResponse } from '@/dtos/response/report.response';
+import { IdReportResonse, ResidentialReportResponse, ResidentialResponse, WorkExpReportResponse, WorkPeerReportResponse } from '@/dtos/response/report.response';
 import { ErrorEnum } from '@/exceptions/errorCodes';
 import { HttpException } from '@/exceptions/httpException';
 import { Document, DocumentModel } from '@/models/document.model';
@@ -157,6 +157,32 @@ class ReportService {
     return idReportResponse;
   }
 
+  public async getVerificationStatus(peers : WorkPeerReportResponse[]){
+    let hasPending = false;
+    let hasAccepted = false;
+    let hasRejected = false;
+  
+    for (const peer of peers) {
+      if (peer.isReal.state === 'PENDING') {
+        hasPending = true;
+      } else if (peer.isReal.state === 'ACCEPTED') {
+        hasAccepted = true;
+      } else if (peer.isReal.state === 'REJECTED') {
+        hasRejected = true;
+      }
+    }
+  
+    if (hasPending) {
+      return 'PENDING';
+    } else if (hasAccepted) {
+      return 'NOT VERIFIED';
+    } else if (hasRejected) {
+      return 'PARTIALLY VERIFIED';
+    } else {
+      return 'UNKNOWN';
+    }
+  }
+
   public async getAllDetails(email?: string, phone?: string, grnID?: string) {
     if (!email && !phone && !grnID) throw new HttpException(ErrorEnum.VALIDATION_ERROR, 'Query Param either email, phone or GreenieID is required');
     
@@ -173,12 +199,18 @@ class ReportService {
       userID = profile.user.toString();
       
     }
+    const accountDetails = await this.getGreenieAccountDetails(userID);
+    const workExperienceDetails = await this.getWorkExperienceDetails(userID);
+    const ResidentialDetails = await this.getResidentialDetails(userID);
+    const idDetails =  await this.getIdDetails(userID);
+    const workExpverificationStatus = await this.getVerificationStatus(workExperienceDetails.peers)
 
     return {
-      accountDetails: await this.getGreenieAccountDetails(userID),
-      workExperienceDetails: await this.getWorkExperienceDetails(userID),
-      ResidentialDetails: await this.getResidentialDetails(userID),
-      idDetails: await this.getIdDetails(userID),
+      accountDetails : accountDetails,
+      workExperienceDetails : workExperienceDetails,
+      ResidentialDetails : ResidentialDetails,
+      idDetails : idDetails,
+      workExpverificationStatus : workExpverificationStatus
     };
   }
 }
